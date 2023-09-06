d8cec55d5a2bd4d442fd064fc1b532ebd42bada5
Platypeak <126321053+Platypeak@users.noreply.github.com>
2023-09-06 17:58:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+# Imports
+import tkinter
+from tkinter import filedialog
+
+def open_file(text):
+    file_path = filedialog.askopenfilename()
+    with open(file_path, "r") as file: # mistake happened here
+        text.delete(1.0, tkinter.END)
+        text.insert(tkinter.END, file.read())
+
+def save_file(text):
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt")
+    with open(file_path, 'w') as file:
+        file.write(text.get(1.0, tkinter.END))
+
+root = tkinter.Tk()
+root.title("Text Editor")
+
+text = tkinter.Text(root)
+text.pack()
+
+menu = tkinter.Menu()
+root.config(menu=menu)
+
+file_menu = tkinter.Menu(menu)
+menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Open", command=lambda: open_file(text))
+file_menu.add_command(label="Save", command=lambda: save_file(text))
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+
+root.mainloop()
